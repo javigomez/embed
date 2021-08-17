7b346052c078f5f01d47dfad025de9583c41627c
antoniofull <antonio.fullone@typeform.com>
2021-08-17 10:34:26 +0200

    fix(DIST-1127): Add hex to Rgb lib

---
+++ packages/embed/src/utils/hex-rgb.spec.ts
@@ -0,0 +1,20 @@
+import { hexRgb } from './hex-rgb'
+
+describe('#hexRGB', () => {
+  it('Returns the correct rgb values for 6 digits hex', () => {
+    expect(hexRgb('#000000')).toEqual({
+      red: 0,
+      green: 0,
+      blue: 0,
+      alpha: 1,
+    })
+  })
+  it('Returns the correct rgb values 3 digits hex', () => {
+    expect(hexRgb('#000')).toEqual({
+      red: 0,
+      green: 0,
+      blue: 0,
+      alpha: 1,
+    })
+  })
+})
