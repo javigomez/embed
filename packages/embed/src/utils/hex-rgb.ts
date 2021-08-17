0b4e060018990d5db4c504bdc9e402a5bb4bd8ea
antoniofull <antonio.fullone@typeform.com>
2021-08-17 10:43:30 +0200

    fix(DIST-1127): Remove unused dependencies

--- packages/embed/src/utils/hex-rgb.ts
+++ packages/embed/src/utils/hex-rgb.ts
@@ -1,5 +1,5 @@
 /**
- * @copyright @sindresorhus 
+ * @copyright @sindresorhus
  * @Github https://github.com/sindresorhus/hex-rgb
  * @NPM: https://www.npmjs.com/package/hex-rgb
  */
@@ -10,26 +10,18 @@ const match6or8Hex = `#?[${hexCharacters}]{6}([${hexCharacters}]{2})?`
 const nonHexChars = new RegExp(`[^#${hexCharacters}]`, 'gi')
 const validHexSize = new RegExp(`^${match3or4Hex}$|^${match6or8Hex}$`, 'i')
 
-type Options = {
-  format: 'array' | 'css'
-  alpha: number
-}
-
-export const hexRgb = (hex: string, options?: Options) => {
+export const hexRgb = (hex: string) => {
   if (typeof hex !== 'string' || nonHexChars.test(hex) || !validHexSize.test(hex)) {
     throw new TypeError('Expected a valid hex string')
   }
 
   hex = hex.replace(/^#/, '')
-  let alphaFromHex = 1
 
   if (hex.length === 8) {
-    alphaFromHex = Number.parseInt(hex.slice(6, 8), 16) / 255
     hex = hex.slice(0, 6)
   }
 
   if (hex.length === 4) {
-    alphaFromHex = Number.parseInt(hex.slice(3, 4).repeat(2), 16) / 255
     hex = hex.slice(0, 3)
   }
 
@@ -41,18 +33,6 @@ export const hexRgb = (hex: string, options?: Options) => {
   const red = number >> 16
   const green = (number >> 8) & 255
   const blue = number & 255
-  const alpha = options && typeof options.alpha === 'number' ? options.alpha : alphaFromHex
 
-  if(options) {
-    if (options.format === 'array') {
-      return [red, green, blue, alpha]
-    }
-    if (options.format === 'css') {
-      const alphaString = alpha === 1 ? '' : ` / ${Number((alpha * 100).toFixed(2))}%`
-      return `rgb(${red} ${green} ${blue}${alphaString})`
-    }
-  }
-
-
-  return { red, green, blue, alpha }
+  return { red, green, blue }
 }
